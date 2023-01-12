57aa5bb1f2835da79b30fa230473b24edcb08d13
Hedi M. Sharif <60695654+HediMuhamad@users.noreply.github.com>
2023-01-12 21:19:19 +0300

    Add(PFM-16): Creating Transaction Component

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,3 +1,55 @@
-export default function Index() {
-  return <div className="index-page"></div>
+import Transaction from "~/components/transaction"
+import styles from "~/styles/pages/overview.css"
+
+export const links = () => {
+  return [{ rel: "stylesheet", href: styles }]
+}
+
+export default function Overview() {
+  return (
+    <div className="overview-page">
+      <Transaction
+        category="ACCESSORIES"
+        title="12 Rules for life by Jordan Peterson signed by himself..."
+        date={new Date()}
+        amount="45600"
+        type="income"
+      />
+      <Transaction
+        category="BOOK"
+        title="12 Rules for life by Jordan Peterson signed by himself..."
+        date={new Date()}
+        amount="45600"
+        type="income"
+      />
+      <Transaction
+        category="SALARY"
+        title="12 Rules for life by Jordan Peterson signed by himself..."
+        date={new Date()}
+        amount="45600"
+        type="income"
+      />
+      <Transaction
+        category="ACCESSORIES"
+        title="12 Rules for life by Jordan Peterson signed by himself..."
+        date={new Date()}
+        amount="45600"
+        type="expense"
+      />
+      <Transaction
+        category="BOOK"
+        title="12 Rules for life by Jordan Peterson signed by himself..."
+        date={new Date()}
+        amount="45600"
+        type="expense"
+      />
+      <Transaction
+        category="SALARY"
+        title="12 Rules for life by Jordan Peterson signed by himself..."
+        date={new Date()}
+        amount="45600"
+        type="expense"
+      />
+    </div>
+  )
 }
