082323e25dcadfb7fe8a201efba699efdfe52a74
Hedi M. Sharif <60695654+HediMuhamad@users.noreply.github.com>
2023-01-17 01:04:25 +0300

    Add(PFM-30): Creating Searchbar Component (#23)

---
+++ app/components/searchbar/index.jsx
@@ -0,0 +1,66 @@
+import PropTypes from "prop-types"
+import { SearchIcon } from "~/shared/assets/"
+import Button from "../button"
+import { useRef } from "react"
+
+export default function Searchbar({
+  nameInForm,
+  value,
+  changeHandler,
+  iconOnClickHandler,
+}) {
+  const inputRef = useRef(null)
+
+  return (
+    <div className="searchbar">
+      <div className="searchbar__input-container">
+        <span
+          className={`searchbar__input-container__icon-container ${
+            iconOnClickHandler
+              ? `searchbar__input-container__icon-container--clickable-yes`
+              : ""
+          }`}
+          onClick={iconOnClickHandler}
+        >
+          <SearchIcon />
+        </span>
+        <input
+          ref={inputRef}
+          name={nameInForm}
+          className="searchbar__input-container__input"
+          type={"text"}
+          placeholder="Search"
+          value={value}
+          onChange={(e) => {
+            changeHandler(e.target.value)
+          }}
+        />
+      </div>
+      <Button
+        className="searchbar__clear-button"
+        buttonSize={"large"}
+        buttonType={"normal"}
+        buttonIsRounded={false}
+        onClick={() => {
+          inputRef.current.value = ""
+        }}
+      >
+        clear
+      </Button>
+    </div>
+  )
+}
+
+Searchbar.propTypes = {
+  nameInForm: PropTypes.string,
+  value: PropTypes.string,
+  changeHandler: PropTypes.func,
+  iconOnClickHandler: PropTypes.func,
+}
+
+Searchbar.defaultProps = {
+  nameInForm: "",
+  value: null,
+  changeHandler: () => {},
+  iconOnClickHandler: null,
+}
